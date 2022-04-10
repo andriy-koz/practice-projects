e3680f440df964a73cb5feb6b9aabf51a097e0e1
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-04-10 18:21:04 -0300

    customize-components-with-theme-provider

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,52 +4,80 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { AccessTime } from '@mui/icons-material';
 import Rating from '@mui/material/Rating';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: 'body2',
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: 'body3',
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={6}>
-      <Paper elevation={3}>
-        <img
-          src='https://static.wixstatic.com/media/b20ea8_d1e33e6255fb46b38d8a3706b681d124~mv2.jpeg/v1/fill/w_940,h_705,al_c,q_90/b20ea8_d1e33e6255fb46b38d8a3706b681d124~mv2.webp'
-          alt=''
-          className='img'
-        />
-        <Box paddingX={1}>
-          <Typography variant='subtitle1' component='h2'>
-            Immerse into the Falls
-          </Typography>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-            }}>
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant='body2' component='p' marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            src='https://static.wixstatic.com/media/b20ea8_d1e33e6255fb46b38d8a3706b681d124~mv2.jpeg/v1/fill/w_940,h_705,al_c,q_90/b20ea8_d1e33e6255fb46b38d8a3706b681d124~mv2.webp'
+            alt=''
+            className='img'
+          />
+          <Box paddingX={1}>
+            <Typography variant='subtitle1' component='h2'>
+              Immerse into the Falls
             </Typography>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+              }}>
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant='body2' component='p' marginLeft={0.5}>
+                5 hours
+              </Typography>
+            </Box>
+            <Box marginTop={3} sx={{ display: 'flex', alignItems: 'center' }}>
+              <Rating
+                name='read-only'
+                value={4.5}
+                readOnly
+                precision={0.5}
+                size='small'
+              />
+              <Typography variant='body2' component='p' marginLeft={0.5}>
+                4.5
+              </Typography>
+              <Typography variant='body3' component='p' marginLeft={1.5}>
+                (655 reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant='h6' component='h3' marginTop={0}>
+                From C $147
+              </Typography>
+            </Box>
           </Box>
-          <Box marginTop={3} sx={{ display: 'flex', alignItems: 'center' }}>
-            <Rating
-              name='read-only'
-              value={4.5}
-              readOnly
-              precision={0.5}
-              size='small'
-            />
-            <Typography variant='body2' component='p' marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant='body3' component='p' marginLeft={1.5}>
-              (655 reviews)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant='h6' component='h3' marginTop={0}>
-              From C $147
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };

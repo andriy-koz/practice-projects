0ac95a3defd5e69e3909c7fb018753bcc7bcdddf
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-04-10 19:38:55 -0300

    added-page-with-different-components

---
+++ src/components/Modal.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 'auto',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function BasicModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>BOOK NOW</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'>
+        <Box sx={style}>
+          <TextField
+            id='datetime-local'
+            label='Next appointment'
+            type='datetime-local'
+            defaultValue='2017-05-24T10:30'
+            sx={{ width: 250 }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,3 +1,51 @@
-const Tour = () => <div>Hello</div>;
+import Container from '@mui/material/Container';
+import CustomizedAccordions from '../components/Accordion';
+import ImageCollage from '../components/ImageCollage';
+import Paper from '@mui/material/Paper';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BasicModal from '../components/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const Tour = () => (
+  <Container sx={{ width: 900 }}>
+    <Typography variant='h3' component='h1' marginTop={3}>
+      Explore the World in Vegas
+    </Typography>
+    <Box marginTop={3} sx={{ display: 'flex' }}>
+      <img
+        src='https://assistotuviaje.com/wp-content/uploads/2021/08/TSV-Destinos-Jun-Art-12-Foto-2-1030x579.jpg'
+        alt=''
+        height={325}
+      />
+      <ImageCollage />
+    </Box>
+    <Box>
+      <Typography variant='h6' component='h4' marginTop={3}>
+        About this ticket
+      </Typography>
+      <Typography variant='paragraph' component='p' marginTop={3}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, distinctio
+        quo sequi aliquid modi asperiores nemo officiis similique, mollitia
+        reprehenderit soluta itaque dicta deserunt commodi odit voluptatum
+        doloribus! Quod sint sequi saepe in iste veniam fugiat aspernatur
+        dolores eum porro!
+      </Typography>
+    </Box>
+    <Box marginBottom={10}>
+      <Typography variant='h6' component='h4' marginTop={3} marginBottom={2}>
+        Frecuently Asked Questions
+      </Typography>
+      <CustomizedAccordions />
+    </Box>
+    <Paper
+      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+      elevation={3}>
+      <BottomNavigation>
+        <BasicModal />
+      </BottomNavigation>
+    </Paper>
+  </Container>
+);
 
 export default Tour;
